dbb5889c4d4ff71f30e3559f5e7d0f0f77a05669
Zuhaitz Martínez <zuhaitzmartinezjimenez@gmail.com>
2024-11-25 12:54:38 +0100

    options api version

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,46 @@
+<script>
+  export default {
+    data() {
+      return {
+        name: "John Doe",
+        status: "active",
+        tasks: ["task 1", "task 2", "task 3"],
+        link: "https://www.google.com",
+      };
+    },
+
+    methods: {
+      toggleStatus() {
+        if (this.status === "active") {
+          this.status = "pending";
+        } else if (this.status === "pending") {
+          this.status = "inactive";
+        } else {
+          this.status = "active";
+        }
+      },
+    },
+  };
+</script>
+
 <template>
-  <h1>Vue Jobs</h1>
+  <h1>Vue Jobs {{ name }}</h1>
+  <p v-if="status === 'active'">User is active</p>
+  <p v-else-if="status === 'pending'">User is pending</p>
+  <p v-else>User is inactive</p>
+
+  <h3>Tasks</h3>
+  <ul>
+    <li v-for="task in tasks" :key="task" @click="() => console.log(task)">
+      {{ task }}
+    </li>
+  </ul>
+
+  <!-- <a v-bind:href="link">Click for Google</a> -->
+  <a :href="link">Click for Google</a>
+
+  <br />
+
+  <!-- <button v-on:click="toggleStatus">Change Status</button> -->
+  <button @click="toggleStatus">Change Status</button>
 </template>
