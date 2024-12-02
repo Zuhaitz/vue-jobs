d72873cab6649ca616cee9ae55d170cae6012a09
Zuhaitz Martínez <zuhaitzmartinezjimenez@gmail.com>
2024-12-02 12:38:35 +0100

    jobs list component base

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,14 @@
   import Navbar from "./components/Navbar.vue";
   import Hero from "./components/Hero.vue";
   import HomeCards from "./components/HomeCards.vue";
+  import JobListings from "./components/JobListings.vue";
 </script>
 
 <template>
   <Navbar />
   <Hero title="Test Title" subtitle="Test Subtitle" />
   <HomeCards />
+  <JobListings />
 </template>
 
 <style lang="scss" scoped></style>
---
+++ src/components/JobListings.vue
@@ -0,0 +1,21 @@
+<script setup>
+  import { ref } from "vue";
+  import jobData from "../jobs.json";
+
+  const jobs = ref(jobData);
+</script>
+
+<template>
+  <section class="px-4 py-10 bg-blue-50">
+    <div class="container-xl lg:container m-auto">
+      <h2 class="mb-6 text-3xl font-bold text-center text-green-500">
+        Browse Jobs
+      </h2>
+      <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
+        <div v-for="job in jobs" :key="job.id">
+          {{ job.title }}
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
